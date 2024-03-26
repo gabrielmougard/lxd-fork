876dafbceee7ef2eabe619e27aaf817cdd053074
Gabriel Mougard <gabriel.mougard@canonical.com>
2024-03-26 09:39:48 +0100

    lxd/instance/drivers/wasmer/test: Add `Limits`, `Memory`, `MemoryType` and `Module` unit tests
    Signed-off-by: Gabriel Mougard <gabriel.mougard@canonical.com>

--- lxd/instance/drivers/wasmer/wasmer_test.go
+++ lxd/instance/drivers/wasmer/wasmer_test.go
@@ -1022,3 +1022,330 @@ func TestImportTypeForMemoryType(t *testing.T) {
 	assert.Equal(t, limitsAgain.Minimum(), minimum)
 	assert.Equal(t, limitsAgain.Maximum(), maximum)
 }
+
+// Limits tests
+
+func TestLimits(t *testing.T) {
+	var minimum uint32 = 1
+	var maximum uint32 = 7
+
+	limits, err := NewLimits(minimum, maximum)
+	assert.NoError(t, err)
+	assert.Equal(t, limits.Minimum(), minimum)
+	assert.Equal(t, limits.Maximum(), maximum)
+}
+
+// Memory tests
+
+func TestMemory(t *testing.T) {
+	engine := NewEngine()
+	store := NewStore(engine)
+	limits, err := NewLimits(1, 7)
+	assert.NoError(t, err)
+
+	_ = NewMemory(store, NewMemoryType(limits))
+}
+
+func TestMemoryGetType(t *testing.T) {
+	engine := NewEngine()
+	store := NewStore(engine)
+	limits, err := NewLimits(1, 7)
+	assert.NoError(t, err)
+
+	memory := NewMemory(store, NewMemoryType(limits))
+
+	limitsAgain := memory.Type().Limits()
+	assert.Equal(t, limitsAgain.Minimum(), uint32(1))
+	assert.Equal(t, limitsAgain.Maximum(), uint32(7))
+}
+
+func TestMemorySize(t *testing.T) {
+	engine := NewEngine()
+	store := NewStore(engine)
+	limits, err := NewLimits(1, 3)
+	assert.NoError(t, err)
+
+	memory := NewMemory(store, NewMemoryType(limits))
+	size := memory.Size()
+	assert.Equal(t, size, Pages(1))
+
+	success := memory.Grow(Pages(2))
+	assert.True(t, success)
+
+	size = memory.Size()
+	assert.Equal(t, size, Pages(3))
+
+	success = memory.Grow(Pages(1))
+	assert.False(t, success)
+}
+
+func TestMemoryDataSize(t *testing.T) {
+	engine := NewEngine()
+	store := NewStore(engine)
+	limits, err := NewLimits(1, 7)
+	assert.NoError(t, err)
+
+	memory := NewMemory(store, NewMemoryType(limits))
+	dataSize := memory.DataSize()
+	assert.Equal(t, dataSize, uint(0x10000))
+}
+
+func TestMemoryData(t *testing.T) {
+	instance := testGetInstance(t)
+
+	getString, err := instance.Exports.GetFunction("string")
+	assert.NoError(t, err)
+
+	ptr, err := getString()
+	assert.NoError(t, err)
+	assert.Equal(t, ptr, int32(1048576))
+
+	pointer := ptr.(int32)
+
+	memory1, err := instance.Exports.GetMemory("memory")
+	assert.NoError(t, err)
+
+	memory2, err := instance.Exports.GetMemory("memory")
+
+	data1 := memory1.Data()
+	data2 := memory2.Data()
+	assert.NoError(t, err)
+	assert.Equal(t, "Hello, World!", string(data1[pointer:pointer+13]))
+	assert.Equal(t, "Hello, World!", string(data2[pointer:pointer+13]))
+
+	data1[pointer] = 'A'
+	assert.Equal(t, "Aello, World!", string(data1[pointer:pointer+13]))
+	assert.Equal(t, "Aello, World!", string(data2[pointer:pointer+13]))
+}
+
+// MemoryType tests
+
+func TestMemoryType(t *testing.T) {
+	var minimum uint32 = 1
+	var maximum uint32 = 7
+	limits, err := NewLimits(minimum, maximum)
+	assert.NoError(t, err)
+
+	memoryType := NewMemoryType(limits)
+	limitsAgain := memoryType.Limits()
+	assert.Equal(t, limitsAgain.Minimum(), minimum)
+	assert.Equal(t, limitsAgain.Maximum(), maximum)
+}
+
+func TestMemoryTypeIntoExternTypeAndBack(t *testing.T) {
+	var minimum uint32 = 1
+	var maximum uint32 = 7
+	limits, err := NewLimits(minimum, maximum)
+	assert.NoError(t, err)
+
+	memoryType := NewMemoryType(limits)
+	externType := memoryType.IntoExternType()
+	assert.Equal(t, externType.Kind(), MEMORY)
+
+	memoryTypeAgain := externType.IntoMemoryType()
+	limitsAgain := memoryTypeAgain.Limits()
+	assert.Equal(t, limitsAgain.Minimum(), minimum)
+	assert.Equal(t, limitsAgain.Maximum(), maximum)
+}
+
+// Module tests
+
+func TestModule(t *testing.T) {
+	engine := NewEngine()
+	store := NewStore(engine)
+	_, err := NewModule(store, []byte("(module)"))
+	assert.NoError(t, err)
+}
+
+func TestValidateModule(t *testing.T) {
+	engine := NewEngine()
+	store := NewStore(engine)
+	err := ValidateModule(store, []byte("(module)"))
+	assert.NoError(t, err)
+}
+
+func TestModuleNameSome(t *testing.T) {
+	engine := NewEngine()
+	store := NewStore(engine)
+	module, err := NewModule(store, []byte("(module $moduleName)"))
+	assert.NoError(t, err)
+
+	name := module.Name()
+	assert.Equal(t, name, "moduleName")
+}
+
+func TestModuleNameNone(t *testing.T) {
+	engine := NewEngine()
+	store := NewStore(engine)
+	module, err := NewModule(store, []byte("(module)"))
+	assert.NoError(t, err)
+
+	name := module.Name()
+	assert.Equal(t, name, "")
+}
+
+func TestModuleImports(t *testing.T) {
+	engine := NewEngine()
+	store := NewStore(engine)
+	module, err := NewModule(
+		store,
+		[]byte(`
+                	(module
+                	  (import "ns" "function" (func))
+                	  (import "ns" "global" (global f32))
+                	  (import "ns" "table" (table 1 2 anyfunc))
+                	  (import "ns" "memory" (memory 3 4)))
+                `),
+	)
+	assert.NoError(t, err)
+
+	imports := module.Imports()
+	assert.Equal(t, len(imports), 4)
+
+	// 0
+	assert.Equal(t, imports[0].Module(), "ns")
+	assert.Equal(t, imports[0].Name(), "function")
+
+	type0 := imports[0].Type()
+	assert.Equal(t, type0.Kind(), FUNCTION)
+
+	functionType := type0.IntoFunctionType()
+	assert.Equal(t, len(functionType.Params()), 0)
+	assert.Equal(t, len(functionType.Results()), 0)
+
+	// 1
+	assert.Equal(t, imports[1].Module(), "ns")
+	assert.Equal(t, imports[1].Name(), "global")
+
+	type1 := imports[1].Type()
+	assert.Equal(t, type1.Kind(), GLOBAL)
+
+	globalType := type1.IntoGlobalType()
+	assert.Equal(t, globalType.ValueType().Kind(), F32)
+	assert.Equal(t, globalType.Mutability(), IMMUTABLE)
+
+	// 2
+	assert.Equal(t, imports[2].Module(), "ns")
+	assert.Equal(t, imports[2].Name(), "table")
+
+	type2 := imports[2].Type()
+	assert.Equal(t, type2.Kind(), TABLE)
+
+	tableType := type2.IntoTableType()
+	tableLimits := tableType.Limits()
+	assert.Equal(t, tableType.ValueType().Kind(), FuncRef)
+	assert.Equal(t, tableLimits.Minimum(), uint32(1))
+	assert.Equal(t, tableLimits.Maximum(), uint32(2))
+
+	// 3
+	assert.Equal(t, imports[3].Module(), "ns")
+	assert.Equal(t, imports[3].Name(), "memory")
+
+	type3 := imports[3].Type()
+	assert.Equal(t, type3.Kind(), MEMORY)
+
+	memoryType := type3.IntoMemoryType()
+	memoryLimits := memoryType.Limits()
+	assert.Equal(t, memoryLimits.Minimum(), uint32(3))
+	assert.Equal(t, memoryLimits.Maximum(), uint32(4))
+}
+
+func TestModuleExports(t *testing.T) {
+	engine := NewEngine()
+	store := NewStore(engine)
+	module, err := NewModule(
+		store,
+		[]byte(`
+			(module
+			  (func (export "function") (param i32 i64))
+			  (global (export "global") i32 (i32.const 7))
+			  (table (export "table") 0 funcref)
+			  (memory (export "memory") 1))
+		`),
+	)
+	assert.NoError(t, err)
+
+	exports := module.Exports()
+	assert.Equal(t, len(exports), 4)
+
+	// 0
+	assert.Equal(t, exports[0].Name(), "function")
+
+	type0 := exports[0].Type()
+	assert.Equal(t, type0.Kind(), FUNCTION)
+
+	functionType := type0.IntoFunctionType()
+	assert.Equal(t, len(functionType.Params()), 2)
+	assert.Equal(t, len(functionType.Results()), 0)
+
+	// 1
+	assert.Equal(t, exports[1].Name(), "global")
+
+	type1 := exports[1].Type()
+	assert.Equal(t, type1.Kind(), GLOBAL)
+
+	globalType := type1.IntoGlobalType()
+	assert.Equal(t, globalType.ValueType().Kind(), I32)
+	assert.Equal(t, globalType.Mutability(), IMMUTABLE)
+
+	// 2
+	assert.Equal(t, exports[2].Name(), "table")
+
+	type2 := exports[2].Type()
+	assert.Equal(t, type2.Kind(), TABLE)
+
+	tableType := type2.IntoTableType()
+	tableLimits := tableType.Limits()
+	assert.Equal(t, tableType.ValueType().Kind(), FuncRef)
+	assert.Equal(t, tableLimits.Minimum(), uint32(0))
+
+	// 3
+	assert.Equal(t, exports[3].Name(), "memory")
+
+	type3 := exports[3].Type()
+	assert.Equal(t, type3.Kind(), MEMORY)
+
+	memoryType := type3.IntoMemoryType()
+	memoryLimits := memoryType.Limits()
+	assert.Equal(t, memoryLimits.Minimum(), uint32(1))
+}
+
+func TestModuleSerialize(t *testing.T) {
+	engine := NewEngine()
+	store := NewStore(engine)
+	_, err := NewModule(store, []byte("(module)"))
+	assert.NoError(t, err)
+}
+
+func TestModuleDeserialize(t *testing.T) {
+	engine := NewEngine()
+	store := NewStore(engine)
+	module, err := NewModule(
+		store,
+		[]byte(`
+			(module
+			  (func (export "function") (param i32 i64)))
+		`),
+	)
+	assert.NoError(t, err)
+
+	serializedModule, err := module.Serialize()
+	assert.NoError(t, err)
+
+	moduleAgain, err := DeserializeModule(store, serializedModule)
+	assert.NoError(t, err)
+
+	exports := moduleAgain.Exports()
+	assert.Equal(t, len(exports), 1)
+	assert.Equal(t, exports[0].Name(), "function")
+
+	type0 := exports[0].Type()
+	assert.Equal(t, type0.Kind(), FUNCTION)
+
+	functionType := type0.IntoFunctionType()
+	assert.Equal(t, len(functionType.Params()), 2)
+	assert.Equal(t, len(functionType.Results()), 0)
+
+	_, err = NewInstance(moduleAgain, NewImportObject())
+	assert.NoError(t, err)
+}
